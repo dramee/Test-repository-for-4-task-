726cf794aba820f9a1941acde5142f6f8f59c70c
dRamee <maksim.yolckin@yandex.com>
2023-02-18 03:16:28 +0700

    First commit

---
+++ test.py
@@ -0,0 +1,19 @@
+a = [12, 32, 35, 41, 52, 67, 90, 112, 131, 315, 123]
+
+
+def search(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        curr_ind = (left + right) // 2
+        if nums[curr_ind] == target:
+            return curr_ind
+        elif nums[curr_ind] < target:
+            left = curr_ind + 1
+        else:
+            right = curr_ind - 1
+    return -1
+
+
+print(search(a, 41))
